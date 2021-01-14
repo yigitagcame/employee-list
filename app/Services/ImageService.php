febe6e821142e371ff4d7d7cb943f9e2e5a3af15
Yigit Agca <yigit@agca.me>
2021-01-14 20:42:15 +0200

    Employee photo path variable added to .env.example

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -8,7 +8,7 @@ class ImageService{
     public function upload($file)
     {
         $name = time().'.'.$file->extension();
-        $folder = '/uploads/';
+        $folder = env('UPLOAD_PHOTO_PATH');
 
         $file->move(public_path($folder), $name);
 
